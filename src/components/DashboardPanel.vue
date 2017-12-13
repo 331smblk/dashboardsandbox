25838855d8c272d8b840597250c2a6573a3ae544
331smblk <brittonlsnyder@gmail.com>
2017-12-12 23:47:41 -0600

    initial

---
+++ src/components/DashboardPanel.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card class="rounded-all">
+        <v-card-media
+          src="src/assets/img/dealershipA.png"
+          height="200px"
+        >
+        </v-card-media>
+        <v-card-title primary-title class="text-xs-left">
+          <div>
+            <div class="headline">Anytown Ford </div>
+            <span class="grey--text">123 Main Street, Kansas City MO 64114</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="blue">Explore</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn icon @click.native="show = !show">
+            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-slide-y-transition>
+          <v-card-text v-show="show">
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </v-slide-y-transition>
+      </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardPanel',
+    data: () => ({
+      
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+
+<style>
+.rounded-all {
+  border-radius: 10px 10px 10px 10px;
+}
+</style>
